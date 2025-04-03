91f197fd29181e2ffb9fb947192f0045b8dff59b
vitus.fire@gmail.com <vitus.fire@gmail.com>
2025-04-03 13:20:18 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,3 +9,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def btn_search_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    query = self.txt_area_query_input.text
+    Notification("Query added!").show()
+    response = f"Here is input:\n{query}"
+    self.txt_area_response.text = response
+    Notification("Done!").show()
